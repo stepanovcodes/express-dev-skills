57c1c27bc2cf55c7fe08b45cbbfb4ce73bff98a8
Alex Stepanov <stepanov.hw@gmail.com>
2023-08-01 23:09:06 -0600

    Finish part 1

--- models/skill.js
+++ models/skill.js
@@ -12,10 +12,20 @@ const skills = [
   ];
   
   module.exports = {
-    getAll
+    getAll,
+    getOne
   };
   
   function getAll() {
     return skills;
+  }
+
+
+  function getOne(id) {
+    // URL params are strings - convert to a number
+    id = parseInt(id);
+    // The Array.prototype.find iterator method is
+    // ideal for finding objects within an array
+    return skills.find(skill => skill.id === id);
   }
   
